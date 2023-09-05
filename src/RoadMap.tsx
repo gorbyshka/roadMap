9cb41b9967243f05e071da9bd53d00d8e146dbd6
GoRbYsKkA <gleb.kovalchuk0609@gmail.com>
2023-09-05 20:48:42 +0300

    sample

--- src/RoadMap.tsx
+++ src/RoadMap.tsx
@@ -1,103 +1,92 @@
+import React, { 
+    useEffect,
+     useRef
+     } from 'react';
+
+import lottie from 'lottie-web';
+import roadmapAnimation from './roadmap.json';
 import styled from "styled-components";
 
 interface roadMapType {
+
     title: string;
     text: string;
-    circleColor: string;
     textColor: string;
+
 }
 
 const roadmapInfo: roadMapType[] = [
     {
         title: "Q2 2021",
         text: "MVP Product Launch in the form of a Telegram bot",
-        circleColor: "#01C3FD",
         textColor: "#01C3FD",
     },
     {
         title: "Q1 2022",
         text: "Cryptodo.app Website Launch",
-        circleColor: "#119BFF",
         textColor: "#119BFF",
     },
     {
         title: "Q2 2023",
         text: "Securing Blockchain Partnerships, Victories in Major",
-        circleColor: "#3398D1",
         textColor: "#3398D1",
     },
     {
         title: "2022-Q2 2023",
         text: "Launch of CryptoDo AI (AI-Powered Custom Smart Contract Builder)",
-        circleColor: "#3354A3",
         textColor: "#3354A3",
     },
     {
         title: "Q3-Q4 2023",
         text: "Seed Investment Round",
-        circleColor: "#6890B9",
         textColor: "#6890B9",
     },
     {
         title: "Q3 2023",
         text: "Launch of CryptoDo AI DApps Audit Solution",
-        circleColor: "#2D609A",
         textColor: "#2D609A",
     },
     {
         title: "Q4 2023",
         text: "Launch of CryptoDo DApp Store",
-        circleColor: "#01C3FD",
         textColor: "#01C3FD",
     },
     {
         title: "Q4 2023",
         text: "Launch of CryptoDo Explorer (Convenient DApps Management Environment)",
-        circleColor: "#119BFF",
         textColor: "#119BFF",
     },
     {
         title: "Q4 2023",
         text: "Public Launch of $CDO Token and Exchange Listing",
-        circleColor: "#3398D1",
         textColor: "#3398D1",
     },
     {
         title: "Q1 2024",
         text: "Launch of CryptoDo Cryptocurrency Payment Gateway",
-        circleColor: "#3354A3",
         textColor: "#3354A3",
     },
     {
         title: "Q2 2024",
         text: "Launch of CryptoDo Developer`s Marketplace",
-        circleColor: "#6890B9",
         textColor: "#6890B9",
     },
     {
         title: "Q2-Q3 2024",
         text: "Launch of CryptoDo Launchpad",
-        circleColor: "#2D609A",
         textColor: "#2D609A",
     },
     {
         title: "Q4 2024",
         text: "Launch of CryptoDo V3 (Visual Web Application Builder with Blockchain Integration)",
-        circleColor: "#01C3FD",
         textColor: "#01C3FD",
     },
     {
         title: "2025",
         text: "Launch of CryptoDo Cross-Chain Builder (Seamless Interaction of Multiple Blockchain Networks)",
-        circleColor: "#119BFF",
         textColor: "#119BFF",
     },
 ];
-
-const Circle = styled.circle<{ fillColor: string }>`
-    fill: ${(props) => props.fillColor};
-    stroke-width: 2;
-`;
 
 const SvgContainer = styled.svg`
     transform: rotate(90deg);
@@ -107,70 +96,72 @@ const TextContainer = styled.div`
    color: white;
    font-size: 24px;
    white-space: pre-wrap;
-   margin-top: 10px;
+   margin-top: 75px;
+   margin-right: 300px;
 `;
 
 const Title = styled.span`
    font-size: 32px;
    color: ${(props) => props.color};
+   z-index: 2;
 `;
-
-const G = styled.g``;
-
-const Path = styled.path``;
 
 export const RoadMap: React.FC = () => {
 
-    const path = "M2.16748 1.94629V472.188C2.16748 611.547 115.075 724.445 254.445 724.445C393.815 724.445 506.722 611.547 506.722 472.188V254.455C506.722 115.097 619.63 2.19829 759 2.19829C898.37 2.19829 1011.28 115.097 1011.28 254.455V472.188C1011.28 611.547 1124.18 724.445 1263.55 724.445C1402.92 724.445 1515.83 611.547 1515.83 472.188V1.94629";
+    const roadmapRef = useRef(null);
 
+    const path =
+        "M2.16797 1.94629V472.188C2.16797 611.547 115.075 724.445 254.445 724.445C393.816 724.445 506.723 611.547 506.723 472.188V254.455C506.723 115.097 619.63 2.19829 759 2.19829C898.371 2.19829 1011.28 115.097 1011.28 254.455V472.188C1011.28 611.547 1124.19 724.445 1263.56 724.445C1402.93 724.445 1515.83 611.547 1515.83 472.188V1.94629"
     const pathElement = document.createElementNS("http://www.w3.org/2000/svg", "path");
 
     pathElement.setAttribute("d", path);
 
     const pathLength = pathElement.getTotalLength();
 
+    useEffect(() => {
+
+        if (roadmapRef.current) {
+
+            const animation = lottie.loadAnimation({
+
+                container: roadmapRef.current,
+                animationData: roadmapAnimation,
+                renderer: 'svg',
+                loop: true,
+                autoplay: true,
+
+            });
+
+        }
+
+    }, []);
+
     return (
 
-        <SvgContainer
-            xmlns="http://www.w3.org/2000/svg"
-            width="1500"
-            height="1600"
-            viewBox="-10 0 1926 518"
-            fill="none"
-            style={{ transform: 'rotate(90deg)' }}
-        >
+        <>
 
-            <Path
-                d={path}
-                stroke="#7B8793"
-                strokeOpacity="0.26"
-                strokeWidth="3"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeDasharray="6 6"
-            />
+            <SvgContainer
+                width="1400"
+                height="1500"
+                viewBox="-10 0 2126 518"
+                fill="none"
+                style={{ transform: 'rotate(90deg)' }}
+                ref={roadmapRef}
+            >
 
-            {roadmapInfo.map((item, index) => {
-                const progress = index / (roadmapInfo.length - 1);
-                const pathPosition = pathLength * progress;
-                const point = pathElement.getPointAtLength(pathPosition);
+                {roadmapInfo.map((item, index) => {
 
-                return (
+                    const progress = index / (roadmapInfo.length - 1);
+                    const pathPosition = pathLength * progress;
+                    const point = pathElement.getPointAtLength(pathPosition);
 
-                    <G key={index}>
-
-                        <Circle
-                            cx={point.x}
-                            cy={point.y}
-                            r="10"
-                            fillColor={item.circleColor}
-                        />
+                    return (
 
                         <foreignObject
                             x={point.x + 15}
                             y={point.y}
-                            width="200"
-                            height="100"
+                            width="500"
+                            height="300"
                             transform={`rotate(270 ${point.x + 15} ${point.y})`}
                         >
 
@@ -190,14 +181,14 @@ export const RoadMap: React.FC = () => {
 
                         </foreignObject>
 
-                    </G>
+                    );
 
-                );
+                })}
 
-            })}
+            </SvgContainer>
 
-        </SvgContainer>
-        
+        </>
+
     );
 
 }
